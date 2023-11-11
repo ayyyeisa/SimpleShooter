38f2e22226f1a89d2c930eade879332c7fec72a8
Isa <80214167+ayyyeisa@users.noreply.github.com>
2023-11-10 19:38:04 -0600

    started set-up

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+/*****************************************************************************
+// File Name : PlayerController.cs
+// Author : Isa Luluquisin
+// Creation Date : November 9, 2023
+//
+// Brief Description : This controls funtions like starting the game using the spacebar,
+//                     as well as quitting and restarting using esc, 'r', and enter respectively.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
